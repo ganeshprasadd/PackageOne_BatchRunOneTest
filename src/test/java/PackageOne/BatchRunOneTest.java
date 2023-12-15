e0fa0625af9e59dbfd6021b6a20cb62f46cef2bf
ganeshprasadd <ganesh.prasadd1199@gmail.com>
2023-12-15 23:56:37 +0530

    1ST Push to Git

---
+++ src/test/java/PackageOne/BatchRunOneTest.java
@@ -0,0 +1,20 @@
+package PackageOne;
+
+import org.testng.annotations.Test;
+
+public class BatchRunOneTest {
+	
+	@Test 
+	public void pushBatchRunOneTest() {
+		
+		System.out.println("Firtst Method belong to Project 1 - Batch First Run");
+		
+	}
+	
+	public void pushBatchRunOneMethodTest() {
+		
+		System.out.println("Second Method belong to Project 1 - Batch First Run");
+		
+	}
+
+}
